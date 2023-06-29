9b62f1d8c323226fd731c98b9a4e152d6ce83eed
Subbu-battala <subbubattala01@gmail.com>
2023-06-29 10:38:00 +0530

    class2

---
+++ src/component/Ex1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Ex1 (props) {
+    return (
+        <div>
+            <h3>States in functional components</h3>
+        </div>
+    )
+}
+
+export default Ex1
